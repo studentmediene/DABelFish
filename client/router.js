c78ba8f00b9ffb8ac5fb6dfab56c6e07d480c1a9
Boye Borg Nygård <boye.borg@gmail.com>
2016-01-06 17:32:05 +0100

    added basic router config

--- client/router.js
+++ client/router.js
@@ -1,1 +1,29 @@
 /* Client-side router settings */
+
+Router.configure({
+  layoutTemplate:"layout"
+});
+
+Router.route("/", {
+  name:"home",
+  template:"home"
+});
+
+Router.route("/log", {
+  name:"log",
+  template:"log"
+});
+
+Router.route("/admin", {
+  name:"admin",
+  template:"admin"
+});
+
+// Probably insecure
+Router.onBeforeAction(function() {
+  if (!Meteor.userId()) {
+    this.render("login");
+  } else {
+    this.next();
+  }
+});
