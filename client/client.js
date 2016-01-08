a408e8e8c2b875d9eb0d555d42e48bd64e3f23e6
Boye Borg Nygård <boye.borg@gmail.com>
2016-01-08 03:42:55 +0100

    subscribed to collections and configured s-alert

--- client/client.js
+++ client/client.js
@@ -1,1 +1,12 @@
 /* Client-side code */
+Meteor.startup(function(){
+  Session.setDefault("DABText_skip", 0);
+});
+
+Meteor.subscribe("DABText");
+Meteor.subscribe("allUserData");
+
+sAlert.config({
+  effect: 'slide',
+  position: 'bottom-right'
+});
