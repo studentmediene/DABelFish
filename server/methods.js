d82362a9a9c17c7bcbb705096339c13782763c57
Boye Borg Nygård <boye.borg@gmail.com>
2016-01-08 03:45:10 +0100

    added methods for adding user, setting password and adding to role

--- server/methods.js
+++ server/methods.js
@@ -1,1 +1,31 @@
 /* Shared server-side methods */
+
+Meteor.methods({
+  addUser: function(user, password) {
+    user.createdByID = this.userId;
+    user.createdBy = Meteor.user().profile.name;
+
+    // Produces better errors than Meteor.runRestricted
+    Security.can(this.userId).insert(user).for(Meteor.users).throw();
+
+    if(!!Meteor.users.findOne({username: user.username})){
+      throw new Meteor.Error(400, "Username is taken");
+    }
+
+    userId = Meteor.users.insert(user);
+    
+    if(userId) {
+      Accounts.setPassword(userId, password);
+    }
+  },
+  setPassword: function(userId, password) {
+    Meteor.runRestricted(function() {
+      Accounts.setPassword(userId, password);
+    });
+  },
+  makeAdmin: function(userId) {
+    Meteor.runRestricted(function() {
+      Roles.addUsersToRoles(userId, "admin");
+    });
+  }
+});
