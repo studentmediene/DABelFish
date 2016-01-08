e24bcf802c3a5247eb450b940d3b796207560687
Boye Borg Nygård <boye.borg@gmail.com>
2016-01-08 03:39:14 +0100

    added custom login field

--- client/pages/login/login.js
+++ client/pages/login/login.js
@@ -1,8 +1,13 @@
-Template.login.helpers({
-  atDisabled: function() {
-    return AccountsTemplates.disabled();
-  },
-  atClass: function() {
-    return AccountsTemplates.disabled() ? 'disabled' : 'active';
+Template.login.events({
+  "submit #login": function(event, template){
+    var username = event.target.username.value;
+    var password = event.target.password.value;
+    sAlert.closeAll(); // close all previous errors
+    Meteor.loginWithPassword({username: username}, password, function(error) {
+      if (error) {
+        sAlert.error('Wrong username or password');
+      }
+    });
+    return false;
   }
 });
