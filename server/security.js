d145b5167fc2d769ca2e70ce8047ffdc45148242
Boye Borg Nygård <boye.borg@gmail.com>
2016-01-08 12:06:15 +0100

    improved security

--- server/security.js
+++ server/security.js
@@ -1,9 +1,11 @@
-Security.defineMethod("isAdmin", {
-  fetch: [],
-  transform: null,
-  deny: function (type, arg, userId) {
-    return !Roles.userIsInRole(userId, 'admin');
-  }
-});
+// Premit admins to create and modify users
+Meteor.users.permit(['insert', 'update']).ifHasRole('admin').apply();
 
-Meteor.users.permit(['insert', 'update', 'remove']).ifHasRole('admin').apply();
+// Never remove a user
+Meteor.users.permit(['remove']).never().apply();
+
+// Logged in users can create records in DABText
+DABText.permit(['insert']).ifLoggedIn().apply();
+
+// Never change or delete a record in DABText
+DABText.permit(['update', 'remove']).never().apply();
