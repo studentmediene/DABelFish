1abe4c8133893260b7be8422123e0b47f1f934eb
Boye Borg Nygård <boye.borg@gmail.com>
2016-01-08 03:41:57 +0100

    published collections and added initial user to database

--- server/server.js
+++ server/server.js
@@ -1,1 +1,28 @@
 /* Server-side code */
+
+Meteor.publish("allUserData", function() {
+  return Meteor.users.find();
+});
+
+Meteor.publish("DABText", function() {
+  return DABText.find({}, {
+    //skip: Session.get("DABText_skip"),
+    limit: 30
+  });
+});
+
+Meteor.startup(function(){
+  if(!Meteor.users.findOne()) {
+    console.log("Found no users in the database, creating the default user.");
+    userId = Meteor.users.insert({
+      username: "radioteknisk@studentmediene.no",
+      roles: ["admin"],
+      profile: {
+        name: "Radioteknisk"
+      },
+      createdBy: "System_1337",
+      createdByID: Random.id()
+    });
+    Accounts.setPassword(userId, "secret");
+  }
+});
