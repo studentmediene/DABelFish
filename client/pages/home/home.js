99b7ee1720249449d328935e36fb05549fe153ad
Boye Borg Nygård <boye.borg@gmail.com>
2016-01-09 14:57:02 +0100

    added DAB text features to the home page, fixes #4

--- client/pages/home/home.js
+++ client/pages/home/home.js
@@ -1,1 +1,73 @@
 /* Controller for home page */
+
+Template.home.rendered = function(){
+  Session.set("dabTextCursor", 0);
+}
+
+
+Template.home.helpers({
+  textLimit: function() {
+    return 140;
+  },
+  currentText: function() {
+    return DABText.findOne({}, {
+      sort: {createdAt: -1}
+    });
+  },
+  formatDate: function(date) {
+    return moment(date).calendar(null, {
+      sameDay: '[Today at] HH:mm',
+      lastDay: '[Yesterday at] HH:mm',
+      lastWeek: '[Last] dddd [at] HH:mm',
+      thisWeek: 'dddd [at] HH:mm',
+      sameElse: 'dddd DD/MM/YY [at] HH:mm'
+    });
+  },
+  texts: function() {
+    cursor = Session.get("dabTextCursor");
+    return DABText.find({},
+    {
+      sort: {createdAt: -1},
+      skip: cursor || 0,
+      limit: 20
+    });
+  },
+  paginationNewerDisabled: function() {
+    if(Number(Session.get("dabTextCursor")) < 20)
+      return "disabled";
+    return "";
+  },
+  paginationOlderDisabled: function() {
+    if(Number(Session.get("dabTextCursor")) >= DABText.find().count() - 20)
+      return "disabled";
+    return "";
+  }
+});
+
+Template.home.events({
+  "submit #new-text": function(event, template) {
+    Meteor.call("addText", {
+      text: event.target.text.value
+    }, function(error, result){
+      if(error){
+        sAlert.error(error.reason);
+        console.log("error", error);
+      } else {
+        sAlert.success("Success!");
+        event.target.text.value = "";
+      }
+    });
+    return false;
+  },
+  "click #pagination-newer": function(event, template) {
+    var cursor = Number(Session.get("dabTextCursor"));
+    if(cursor >= 20)
+      Session.set("dabTextCursor", cursor - 20);
+  },
+  "click #pagination-older": function(event, template) {
+    var cursor = Number(Session.get("dabTextCursor"));
+    var count = DABText.find().count() - 20;
+    if(cursor < count)
+      Session.set("dabTextCursor", cursor + 20);
+  }
+});
